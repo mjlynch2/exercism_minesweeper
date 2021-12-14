516c15a2b6a77b8f735831366c879b6cf92b9036
Mike Lynch <mjlynch2@gmail.com>
2021-12-13 22:19:12 -0600

    add initial input validation

--- minesweeper.js
+++ minesweeper.js
@@ -3,6 +3,37 @@
 // convenience to get you started writing code faster.
 //
 
-export const annotate = (input) => {
-  throw new Error('Remove this statement and implement this function');
+const annotate = (input) => {
+  input.forEach((e, i) => {
+    if(isValid(e)){
+      console.log(e);
+    };
+  });
 };
+
+const isValid = (input) => {
+  // board cannot contain invalid characters
+  const regex = /[^\* ]/g;
+  let isValid = true;
+  [...input].forEach((e) => {
+    if(regex.test(e)){
+      console.log(`Error: ${input} contains invalid character(s)`);
+      isValid = false;
+    } 
+    else if (e == ""){
+      console.log(`Error: ${input} contains blank(s)`);
+      isValid = false;
+    }
+  })
+  // board must be rectangular
+  if (input.length < 2 || [...input].length < 1) {
+      console.log(`Error: ${input} is not a rectangle`);
+      isValid = false;
+  }
+  
+  return isValid;
+};
+
+const input = [["*"], [" "], ["  ", " 1"], [" ", ""], [" ", " ", " "]];
+
+annotate(input);
