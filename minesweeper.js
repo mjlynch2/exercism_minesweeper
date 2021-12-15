1b5f9c2863b2a79d8c2bde536386a0742ae5668c
Mike Lynch <mjlynch2@gmail.com>
2021-12-15 13:07:29 -0600

    break main method into helper methods, improve readability

--- minesweeper.js
+++ minesweeper.js
@@ -2,42 +2,25 @@ export const annotate = (input) => {
   if (!isValid(input)) {
     return `[${input}] is invalid`;
   }
-  let width = input[0].length;
-  let height = input.length;
+
+  const board = {
+    input: input,
+    width: input[0].length,
+    height: input.length,
+  };
+
   let result = [];
 
-  for (let y = 0; y < height; y++) {
-    let row = [];
+  for (let y = 0; y < board.height; y++) {
     let rowStr = "";
-    for (let x = 0; x < width; x++) {
+    for (let x = 0; x < board.width; x++) {
       let sum = 0;
-      if (hasMine([...input[y]][x])) {
+      if (hasMine([...board.input[y]][x])) {
         sum = -1;
       } else {
-        for (let i = -1; i < 2; i++) {
-          for (let j = -1; j < 2; j++) {
-            let xCoord = x + i;
-            let yCoord = y + j;
-            if (0 > xCoord || xCoord > width - 1) {
-              continue;
-            } else if (0 > yCoord || yCoord > height - 1) {
-              continue;
-            } else {
-              if (hasMine([...input[yCoord]][xCoord])) {
-                sum++;
-              }
-            }
-          }
-        }
+        sum = sumAdjacent(x, y, board);
       }
-      if (sum > 0) {
-        row.push(sum);
-      } else if ((sum == -1)) {
-        row.push("*");
-      } else {
-        row.push(" ");
-      }
-      rowStr = row.join("");
+      rowStr = buildRow(sum, rowStr);
     }
     result.push(rowStr);
   }
@@ -48,14 +31,45 @@ const hasMine = (val) => {
   return val === "*";
 };
 
+const sumAdjacent = (x, y, board) => {
+  let sum = 0;
+  for (let i = -1; i < 2; i++) {
+    for (let j = -1; j < 2; j++) {
+      let xCoord = x + i;
+      let yCoord = y + j;
+      if (0 > xCoord || xCoord > board.width - 1) {
+        continue;
+      } else if (0 > yCoord || yCoord > board.height - 1) {
+        continue;
+      } else {
+        if (hasMine([...board.input[yCoord]][xCoord])) {
+          sum++;
+        }
+      }
+    }
+  }
+  return sum;
+};
+
+const buildRow = (sum, row) => {
+  let newRow;
+  if (sum > 0) {
+    newRow = row + sum;
+  } else if (sum == -1) {
+    newRow = row + "*";
+  } else {
+    newRow = row + " ";
+  }
+  return newRow;
+};
 
 const isValid = (input) => {
   const regex = /[^\* ]/g;
-  
+
   let isValid = true;
-  
-  if(input.length == 0) {
-  // board can't be empty
+
+  if (input.length == 0) {
+    // board can't be empty
     return false;
   }
   let width = input[0].length;
@@ -63,7 +77,7 @@ const isValid = (input) => {
     if (regex.test(e)) {
       // board cannot contain invalid characters
       isValid = false;
-    } else if (input.length < 1 || (e.length < 2 && input.length == 1) || e.length != width) {
+    } else if ((e.length < 2 && input.length == 1) || e.length != width) {
       // board must be rectangular
       isValid = false;
     }
